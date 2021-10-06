5112c80e49c46e54967184f5c6a7f260e4072431
Alexey <alex967268@gmail.com>
2021-10-06 23:36:14 +0300

    entry page layout (верстка)

--- client/src/components/common/GreenBtn/GreenBtn.jsx
+++ client/src/components/common/GreenBtn/GreenBtn.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import classNames from "classnames"
 import "./GreenBtn.scss"
 
-const GreenBtn = ({ children, disabled, onClick }) => {
-    const styles = classNames("green-btn", {
+const GreenBtn = ({ children, disabled, onClick, className }) => {
+    const styles = classNames("green-btn", className, {
         "btn-disabled": disabled,
     })
 
     return (
         <button onClick={onClick} className={styles}>
-            <span className="green-btn__inner-text">{children}</span>
+            <span class="green-btn__inner-text">{children}</span>
         </button>
     )
 }
--- client/src/components/pages/EntryPage/EntryPage.jsx
+++ client/src/components/pages/EntryPage/EntryPage.jsx
@@ -4,17 +4,22 @@ import "./EntryPage.scss"
 
 const EntryPage = () => {
     return (
-        <div class="entry-page">
+        <div className="entry-page">
             <div className="entry-page__container">
                 <div className="entry-page__welcome">
-                    <h1 className="entry-page__title">Hello dear friend, you can</h1>
+                    <ul className="entry-page__title">
+                        <li>Hello dear friend,</li>
+                        <li>you can</li>
+                    </ul>
                 </div>
                 <div className="entry-page__sign-up entry-sign-up">
                     <div className="entry-sign-up__login">
                         <GreenBtn className="entry-sign-up__login-btn">Login</GreenBtn>
                     </div>
                     <span className="entry-sign-up__separator">or</span>
-                    <div className="entry-sign-up__register"></div>
+                    <div className="entry-sign-up__register">
+                        <GreenBtn className="entry-sign-up__reg-btn">Register</GreenBtn>
+                    </div>
                 </div>
             </div>
         </div>
